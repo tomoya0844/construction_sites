7f6d1d820d13c632aa45f9ff3e51f23502634e33
tomoya koike <dpon3285@yahoo.co.jp>
2024-01-23 22:54:11 +0900

    Memoクラス作成

---
+++ src/main/java/demo/example/entity/Memo.java
@@ -0,0 +1,17 @@
+package demo.example.entity;
+
+public class Memo {
+  private int id;
+
+  public Memo(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+}
